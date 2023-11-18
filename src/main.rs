16c2a153af27311655ffe911edbda953d9a69548
tom <thomasharrywilburn@gmail.com>
2023-11-18 15:14:21 +0000

    initial working version

---
+++ src/main.rs
@@ -0,0 +1,114 @@
+use neuron::{Neuron, BackPropogatable};
+use crate::neuron::Compute;
+//https://www.infoworld.com/article/3685569/how-to-build-a-neural-network-in-java.html
+mod neuron;
+
+fn main() {
+    println!("Welcome to the neural net example!");
+    println!("we will train a neural net to preduct if a new input is a man or a woman based on their weight and height.\n");
+
+    // naive prediction:
+    // let prediction = Network::default().predict(115 as f64, 66 as f64);
+    // println!("prediction:{}", prediction);
+
+    //weight, height
+    let mut data:Vec<Vec<i32>> = Vec::new();
+    data.push(vec![115, 66]);
+    data.push(vec![175, 78]);
+    data.push(vec![205, 72]);
+    data.push(vec![120, 67]);
+    let answers: Vec<f64> = vec![1.0,0.0,0.0,1.0];  
+
+    let mut network = Network::default();
+    network.train(data, answers);
+
+    test(&network);
+}
+
+fn test(network: &Network) {
+    println!("\n  male: 167, 73: {}", network.predict(167.0, 73.0));
+    println!("female: 105, 67: {}", network.predict(105.0, 67.0)); 
+    println!("female: 120, 72: {}", network.predict(120.0, 72.0)); 
+    println!("  male: 143, 67: {}", network.predict(143.0, 67.0));
+    
+    //print outlier
+    println!("  male', 130, 66: {} <-- deliberate outlier", network.predict(130.0, 66.0));
+}
+
+//although this is a 1d list, we connect them during usage
+struct Network{
+    neurons: Vec<neuron::Neuron>,
+
+}
+
+impl Default for Network {
+    fn default() -> Self {
+        let neurons = vec![
+            Neuron::default(), Neuron::default(), Neuron::default(), //input nodes
+            Neuron::default(), Neuron::default(), //hidden nodes
+            Neuron::default()]; //output nodes
+        Network { neurons: neurons }
+    }
+}
+trait Learnable {
+    fn predict(&self, input1: f64, input2: f64) -> f64;
+    fn train(&mut self, data: Vec<Vec<i32>>, answers: Vec<f64>);
+}
+
+impl Learnable for Network {
+    fn predict(&self, input1: f64, input2: f64) -> f64 {
+        return self.neurons[5].compute(
+            self.neurons[4].compute(
+                self.neurons[2].compute(input1, input2), 
+                self.neurons[1].compute(input1, input2)
+            ),
+            self.neurons[3].compute(
+                self.neurons[1].compute(input1, input2), 
+                self.neurons[0].compute(input1, input2)
+            )
+        );
+    }
+
+    fn train(&mut self, data: Vec<Vec<i32>>, answers: Vec<f64>) {
+        let mut best_epoch_loss =  -1.0;
+        for epoch in 0..1000 {
+            // adapt neuron
+            self.neurons[epoch % 6].mutate();
+
+            let mut predictions:Vec<f64> = Vec::new();
+
+            for i in 0..data.len() -1 {
+                predictions.push(self.predict(data[i][0] as f64, data[i][1] as f64));
+            }
+
+            let this_epoch_loss = mean_square_loss(&answers, predictions);
+
+            if best_epoch_loss == -1.0 {
+                best_epoch_loss = this_epoch_loss;
+                self.neurons[epoch % 6].remember();
+            } else {
+                if this_epoch_loss < best_epoch_loss {
+                best_epoch_loss = this_epoch_loss;
+                self.neurons[epoch % 6].remember();
+            } else {
+                self.neurons[epoch % 6].forget();
+            }
+
+            //log every 10th training epoch
+            if epoch % 10 == 0{
+                println!("Epoch: {} | bestEpochLoss: {} | thisEpochLoss: {}", epoch, best_epoch_loss, this_epoch_loss);
+            } 
+        }
+    }
+}
+}
+
+fn mean_square_loss(correct_answers: &Vec<f64>, predicted_answers: Vec<f64>) -> f64{
+    let mut sum_square = 0.0;
+    for i in 0..correct_answers.len() -1 {
+      let error = correct_answers[i] - predicted_answers[i];
+      sum_square += error * error;
+    }
+    return sum_square / (correct_answers.len() as f64);
+}
+
---
+++ src/neuron/mod.rs
@@ -0,0 +1,80 @@
+use rand::Rng;
+
+pub struct Neuron {
+    bias: f64,
+    weight1: f64,
+    weight2: f64,
+    old_bias: f64,
+    old_weight1: f64,
+    old_weight2: f64,
+}
+
+impl Default for Neuron {
+    fn default() -> Self {
+        let mut rng = rand::thread_rng();
+        let init_bias = rng.gen_range(-1.0..1.0);
+        let init_weight1 = rng.gen_range(-1.0..1.0);
+        let init_weight2 = rng.gen_range(-1.0..1.0);
+
+        Neuron { bias: init_bias, 
+            weight1: init_weight1, 
+            weight2: init_weight2,
+            old_bias: init_bias.clone(),
+            old_weight1: init_weight1.clone(),
+            old_weight2: init_weight2.clone(),
+         }
+    }
+}
+
+pub trait Compute {
+    fn compute(&self, input1: f64, input2: f64) -> f64;
+}
+
+pub trait BackPropogatable {
+    fn mutate(&mut self);
+    fn forget(&mut self);
+    fn remember(&mut self);
+}
+
+impl Compute for Neuron {
+    fn compute(&self, input1: f64, input2: f64) -> f64 {
+        let pre_activation = 
+        (self.weight1 * input1) + 
+        (self.weight2 * input2) + 
+        self.bias;
+
+        sigmoid(pre_activation)
+    }
+}
+
+impl BackPropogatable for Neuron {
+    fn mutate(&mut self) {
+        let mut rng = rand::thread_rng();
+        let property_to_change = rng.gen_range(0..3);
+        let change_factor = rng.gen_range(-1.0..1.0);
+
+        if property_to_change == 0 { 
+            self.bias += change_factor; 
+        } else if property_to_change == 1 { 
+            self.weight1 += change_factor; 
+        } else { 
+            self.weight2 += change_factor; 
+        };
+    }
+    
+    fn forget(&mut self) {
+        self.bias = self.old_bias;
+        self.weight1 = self.old_weight1;
+        self.weight2 = self.old_weight2;
+    }
+
+    fn remember(&mut self) {
+        self.old_bias = self.bias;
+        self.old_weight1 = self.weight1;
+        self.old_weight2 = self.weight2;
+    }
+}
+
+fn sigmoid(input: f64) -> f64 {
+    1 as f64 / (1 as f64 + std::f64::consts::E.powf(-input))
+}
